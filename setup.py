f106b4b31c83c93431067c0605f17d64c15f293c
wfxey <158351052+wfxey@users.noreply.github.com>
2024-12-26 14:37:34 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='InsightLogger',
-    version='1.2.0',
+    version='1.2.1',
     packages=find_packages(), 
     license='MIT',
     description='A customizable logging utility with enhanced features for developers.',
@@ -14,7 +14,7 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url='https://github.com/VelisCore/InsightLogger',
-    download_url='https://github.com/VelisCore/InsightLogger/archive/refs/tags/v1.2.tar.gz',
+    download_url='https://github.com/VelisCore/InsightLogger/archive/refs/tags/v1.2.1tar.gz',
     keywords=[
         'logging', 'log', 'logger', 'developer tools', 'performance monitoring', 'visualization'
     ],
